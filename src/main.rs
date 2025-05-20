a445ec34814b5177106ebc3a648e0c306d579ff8
SmartConTractor <login.blockchain888@gmail.com>
2025-05-20 18:07:12 +0200

    Add To-do methods.

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,17 @@ fn main() {
 struct Todo {
     map: HashMap<String, bool>,
 }
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
+}
